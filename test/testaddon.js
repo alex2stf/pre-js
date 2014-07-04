c4fb85e7622462cd4d35fdf9524699abb56068d7
alex2stf <alex2stf@yahoo.com>
2014-07-04 19:13:13 +0300

    changes in samples

--- test/testaddon.js
+++ test/testaddon.js
@@ -3,21 +3,21 @@
 //git push -u origin master
 
 
-var preJs = require('../build/Release/sten');
+var sten = require('../build/Release/sten');
 var fs = require('fs');
 
 
-preJs.createContext();
+sten.createContext();
 
 /*basic parser: *****************************************************************************************************************/
 var str = "hi this is some random <?pjs var arr= ['s', 't', 'r', 'i', 'n', 'g'];  print(arr.join('-'))  pjs?> to parse";
 
-preJs.parse(str, function(respone){
+sten.parse(str, function(respone){
 	console.log('__: parsed content:');
 	console.log('js: '+respone);
 });
 
-preJs.destroyContext();
+sten.destroyContext();
 
 
 /*basic parser with global settings: *****************************************************************************************************************/
@@ -26,30 +26,30 @@ var settings = {
 		string: "arguments",
 		data: [1, true, "data"]
 }
-preJs.createContext(settings);
+sten.createContext(settings);
 
 var str2 = "hi this is another sample using <?pjs this.number=4; print(string, ' ',  data[2])  pjs?> and 'this': var a = '<?pjsprint(data[1])pjs?>'  and var b = '<?pjsprint(data[0], this.number)pjs?>';";
 
-preJs.parse(str2, function(respone){
+sten.parse(str2, function(respone){
 	console.log('js: 2nd parsed content:');
 	console.log('js: '+respone);
 });
 
 
-preJs.loadFile('test/workdir/input.html', function(response) {
+sten.loadFile('test/workdir/input.html', function(response) {
     console.log('js:'+response);
     fs.writeFile('test/workdir/output.html', response);
 });
 
 // if the context is not destroyed you have access to variables defined in previous file
-preJs.loadFile('test/workdir/input.css', function(response) {
+sten.loadFile('test/workdir/input.css', function(response) {
 	console.log('js:'+response);
     fs.writeFile('test/workdir/output.css', response);
 
 });
 
 
-var x = preJs.cin("and this a cin method: just type something >> ");
+var x = sten.cin("and this a cin method: just type something >> ");
 console.log('you typed "'+x+'" THANKS!');
 
 
--- test/testmodule.js
+++ test/testmodule.js
@@ -1,7 +1,7 @@
-var prejs = require("../index.js");
+var sten = require("../index.js");
 var str = "hi this is some random <?pjs var arr= ['s', 't', 'r', 'i', 'n', 'g'];  print(arr.join('-'))  pjs?> to parse";
 
-prejs.createContext();
-prejs.parse(str, function(msg) {
+sten.createContext();
+sten.parse(str, function(msg) {
     console.log(msg);
 });
